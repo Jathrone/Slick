af0408a3b62d359c952c56d8b83dd4931809c5bb
Jathrone <jason.li.feed@gmail.com>
2019-05-02 15:25:51 -0700

    add grid and styling to main splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -23,21 +23,32 @@ class Splash extends React.Component {
         }
 
         return (
-            <header className="splash-header">
-                <div className="slick-logo-div">
-                    <Link to={`/`}>slick</Link>
-                </div>
-                <nav className="splash-nav">
-                    <ul className="client-resources-list">
-                        <li>Why Slick?</li>
-                        <li>Solutions</li>
-                        <li>Resouces</li>
-                        <li>Enterprise</li>
-                        <li>Pricing</li>
-                    </ul>
-                    {navbar}
-                </nav>
-            </header>
+            <>
+                <header className="splash-header">
+                    <div className="slick-logo-div">
+                        <Link to={`/`}>slick</Link>
+                    </div>
+                    <nav className="splash-nav">
+                        <ul className="client-resources-list">
+                            <li>Why Slick?</li>
+                            <li>Solutions</li>
+                            <li>Resouces</li>
+                            <li>Enterprise</li>
+                            <li>Pricing</li>
+                        </ul>
+                        {navbar}
+                    </nav>
+                </header>
+                <section className="splash-main">
+                    <div className="splash-grid-container">
+                        <h1>Imagine what you'll accomplish together</h1>
+                        <img className="smallImage" src="https://source.unsplash.com/ZoAlN3-pJO8/251x251" alt="work-coffee"/>
+                        <img className="bigImage" src="https://source.unsplash.com/fznQW-kn5VU/502x502" alt="designers"/>
+                        <img className="midImage" src="https://source.unsplash.com/lp1AKIUV3yo/316x316" alt="consultants"/>
+                        <p className="slick-blurb">Slack is a collaboration hub for work, no matter what work you do. It’s a place where conversations happen, decisions are made, and information is always at your fingertips. With Slack, your team is better connected.</p>
+                    </div>
+                </section>
+            </>
         );
     }
 }
