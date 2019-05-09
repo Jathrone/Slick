d4cc37c730369238e027ae9a2498c81644670fbd
Jathrone <jason.li.feed@gmail.com>
2019-05-08 20:33:46 -0700

    Fix scrollable timestamps on chat

--- frontend/components/workspace/chat_index.jsx
+++ frontend/components/workspace/chat_index.jsx
@@ -1,6 +1,58 @@
+// import React from "react";
+// import ChatItemContainer from "./chat_item_container";
+// import Moment from "react-moment";
+
+// class ChatIndex extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             messageUnderEdit: null
+//         }
+//     }
+    
+//     render() {
+//         const chatIndexDisplay = [];
+//         let lastTimestamp = null;
+//         let lastSenderId = null;
+
+
+//         this.props.messages.forEach((message) => {
+//             let messageTimestamp = new Date(message.createdAt)
+//             if (!lastTimestamp || messageTimestamp.toDateString() !== lastTimestamp.toDateString()) {
+//                 chatIndexDisplay.push(<li key={messageTimestamp.toDateString()} className="workpace-main-chat-timestamp-separator"><Moment date={messageTimestamp} format='dddd, MMMM Do YYYY' /></li>)
+//                 // #TODO allow for more native human process of dates (so-many hours ago, today, yesterday)
+//             } 
+//             if (this.state.messageUnderEdit === message.id) {
+//                 chatIndexDisplay.push(<div>placeholder div for messages under edit</div>)
+//             } 
+//             // else if (message has replies) {
+//                 // #TODO thread messages 
+//             // } 
+//             else if (message.senderId !== lastSenderId) {
+//                 chatIndexDisplay.push(<ChatItemContainer key={message.id} hasMessageHeader={true} message={message} />)
+//             } else if (!lastTimestamp || (messageTimestamp - lastTimestamp) > 1800000) {
+//                 chatIndexDisplay.push(<ChatItemContainer key={message.id} hasMessageHeader={true} message={message} />)
+//             } else {
+//                 chatIndexDisplay.push(<ChatItemContainer key={message.id} hasMessageHeader={false} message={message} />)
+//             }
+//             lastTimestamp = messageTimestamp;
+//             lastSenderId = message.senderId;
+//         })
+
+//         return (
+//             <ul className="workspace-main-chat-list">
+//                 {chatIndexDisplay}
+//             </ul>
+//         )
+
+//     }
+// }
+
+// export default ChatIndex;
+
 import React from "react";
 import ChatItemContainer from "./chat_item_container";
-import Moment from "react-moment";
+import TimeBucket from "./time_bucket";
 
 class ChatIndex extends React.Component {
     constructor(props) {
@@ -9,43 +61,51 @@ class ChatIndex extends React.Component {
             messageUnderEdit: null
         }
     }
-    
+
     render() {
         const chatIndexDisplay = [];
+        let timeBucketContent;
         let lastTimestamp = null;
         let lastSenderId = null;
-
-        const toUpperCaseFilter = (d) => {
-            return d.toUpperCase();
-        };
 
         this.props.messages.forEach((message) => {
             let messageTimestamp = new Date(message.createdAt)
             if (!lastTimestamp || messageTimestamp.toDateString() !== lastTimestamp.toDateString()) {
-                chatIndexDisplay.push(<li key={messageTimestamp.toDateString()} className="workpace-main-chat-timestamp-separator"><Moment date={messageTimestamp} format='dddd, MMMM Do YYYY' /></li>)
+                if (lastTimestamp) {
+                    chatIndexDisplay.push(<TimeBucket key={lastTimestamp.toDateString()} messageTimestamp={lastTimestamp}>
+                        {timeBucketContent}
+                    </TimeBucket>)
+                }
+                timeBucketContent = []
                 // #TODO allow for more native human process of dates (so-many hours ago, today, yesterday)
-            } 
+            }
             if (this.state.messageUnderEdit === message.id) {
-                chatIndexDisplay.push(<div>placeholder div for messages under edit</div>)
-            } 
+                timeBucketContent.push(<div>placeholder div for messages under edit</div>)
+            }
             // else if (message has replies) {
-                // #TODO thread messages 
+            // #TODO thread messages 
             // } 
             else if (message.senderId !== lastSenderId) {
-                chatIndexDisplay.push(<ChatItemContainer key={message.id} hasMessageHeader={true} message={message} />)
+                timeBucketContent.push(<ChatItemContainer key={message.id} hasMessageHeader={true} message={message} />)
             } else if (!lastTimestamp || (messageTimestamp - lastTimestamp) > 1800000) {
-                chatIndexDisplay.push(<ChatItemContainer key={message.id} hasMessageHeader={true} message={message} />)
+                timeBucketContent.push(<ChatItemContainer key={message.id} hasMessageHeader={true} message={message} />)
             } else {
-                chatIndexDisplay.push(<ChatItemContainer key={message.id} hasMessageHeader={false} message={message} />)
+                timeBucketContent.push(<ChatItemContainer key={message.id} hasMessageHeader={false} message={message} />)
             }
             lastTimestamp = messageTimestamp;
             lastSenderId = message.senderId;
         })
+        if (lastTimestamp) {
+            chatIndexDisplay.push(<TimeBucket key={lastTimestamp.toDateString()} messageTimestamp={lastTimestamp}>
+                {timeBucketContent}
+            </TimeBucket>)
+        }
+
 
         return (
-            <ul className="workspace-main-chat-list">
+            <div className="workspace-main-chat-list">
                 {chatIndexDisplay}
-            </ul>
+            </div>
         )
 
     }
---
+++ frontend/components/workspace/time_bucket.jsx
@@ -0,0 +1,33 @@
+// import React from "react";
+// import Moment from "react-moment";
+
+// const TimeBucket = (props) => {
+
+//     return (
+//         <ul className="workspace-main-chat-timestamp-bucket">
+//             <div><Moment date={props.messageTimestamp} format='dddd, MMMM Do YYYY' /></div>
+//             {props.children}
+//         </ul>
+//     )
+// }
+
+// export default TimeBucket;
+
+import React from "react";
+import Moment from "react-moment";
+
+const TimeBucket = (props) => {
+
+    return (
+        <ul className="workspace-main-chat-timestamp-bucket">
+            <div className="scrollable-timestamp">
+            <div className="technically-relatively"><div className="scrollable-timestamp-before"></div></div>
+            <Moment date={props.messageTimestamp} format='dddd, MMMM Do YYYY' />
+            <div className="technically-relatively"><div className="scrollable-timestamp-after"></div></div>
+            </div>
+            {props.children}
+        </ul>
+    )
+}
+
+export default TimeBucket;
