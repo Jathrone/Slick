26389bd72c288a20d69254c04a3ec6771f9bbdf0
Jathrone <jason.li.feed@gmail.com>
2019-05-10 09:58:00 -0700

    add styling to channel creation form

--- frontend/components/workspace/add_channel_form.jsx
+++ frontend/components/workspace/add_channel_form.jsx
@@ -49,24 +49,42 @@ class AddChannelForm extends React.Component {
             )
         } else {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Channel name"
-                    onChange={this.handleChange("name")}
-                    value={this.state.name}/>
-                <input
-                    type="text"
-                    placeholder="Channel topic (max 255 characters)"
-                    onChange={this.handleChange("topic")}
-                    value={this.state.topic} />
-                <input
-                    type="text"
-                    placeholder="Channel purpose"
-                    onChange={this.handleChange("purpose")}
-                    value={this.state.purpose} />
-                <button type="submit">Create Channel</button>
-            </form>
+            <div className="add-channel-page">
+                <div className="escape-div">
+                    <button
+                        type="input"
+                        value="ESC"
+                        onClick={this.props.handleResetActiveArea}>
+                        <i className="fas fa-times"></i>ESC</button>
+                </div>
+                <form onSubmit={this.handleSubmit} className="add-channel-form">
+                    <h1 className="add-channel-header">Create a channel</h1>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        placeholder="Channel name"
+                        id="name"
+                        onChange={this.handleChange("name")}
+                        value={this.state.name}/>
+                    <label htmlFor="topic">Topic (optional)</label>
+                    <input
+                        type="text"
+                        placeholder="Channel topic (max 255 characters)"
+                        id="topic"
+                        onChange={this.handleChange("topic")}
+                        value={this.state.topic} />
+                    <label htmlFor="purpose">Purpose (optional)</label>
+                    <input
+                        type="text"
+                        placeholder="Channel purpose"
+                        id="purpose"
+                        onChange={this.handleChange("purpose")}
+                        value={this.state.purpose} />
+                    <div className="add-channel-button-wrapper">
+                        <button type="submit">Create Channel</button>
+                    </div>
+                </form>
+            </div>
         )
         }
     }
