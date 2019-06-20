71495aef1abf2b6e9a5c15304341197e910c8841
Jathrone <jason.li.feed@gmail.com>
2019-06-20 10:10:35 -0700

    fix hash routing bug in auth routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AuthRoute, ReverseAuthRoute, ReverseAuthRedirectRoute } from "../util/route_util";
+import { AuthRoute, ReverseAuthRoute, ReverseAuthRedirectRoute, AuthRedirectRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 import SplashContainer from "./splash/splash_container";
 import AuthPageWrapperContainer from "./auth_page/auth_page_wrapper_container";
@@ -14,7 +14,10 @@ const App = () => {
             {/* #TODO insert logic for automatic log in of users if user specified the correct route here */}
             <ReverseAuthRedirectRoute path="/"/>
         </Switch>
-        <AuthRoute path="/" component={WorkspaceWrapperContainer}></AuthRoute>
+        <Switch>
+            <AuthRoute exact path="/" component={WorkspaceWrapperContainer}></AuthRoute>
+            <AuthRedirectRoute path="/" />
+        </Switch>
     </>
     );
 }
